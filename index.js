4ed59acb3347195774aa6601bf54bfb036d74b97
Raj <rajmaurya3091@gmail.com>
2026-01-14 23:21:27 +0530

    quora-designing

--- index.js
+++ index.js
@@ -1,42 +1,64 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const port = 8080;
 const path = require('path');
+const { v4:uuidv4 } = require('uuid');
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}));
-app.set("views", path.join(__dirname,"views"))
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("response receive");
 })
 
-app.listen(port,(req,res) =>{
+app.listen(port, (req, res) => {
     console.log("running");
 })
 
 let posts = [
-   {
-    user: "raj",
-    Comment: " Abcd dfg"
-   } ,
+    {
+        id: uuidv4(),
+        user: "ullu",
+        Comment: " baklol dfg"
+    },
 
     {
-    user: "raj",
-    Comment: " Abcd dfg"
-   } ,
+        id: uuidv4(),
+        user: "kallu",
+        Comment: " baal kaatne"
+    },
 
     {
-    user: "raj",
-    Comment: " Abcd dfg"
-   } ,
+        id: uuidv4(),
+        user: "tillu",
+        Comment: " bheekh wala"
+    },
 
     {
-    user: "raj",
-    Comment: " Abcd dfg"
-   } 
+        id: uuidv4(),
+        user: "niddu",
+        Comment: " pres krne wala"
+    }
 ]
-app.get("/post",(req,res)=>{
-    res.render("posts.ejs",{posts});
+app.get("/post", (req, res) => {
+    res.render("posts.ejs", { posts });
+})
+
+app.get("/posts/newPost", (req, res) => {
+    res.render("newPost.ejs");
+})
+
+app.post("/posts", (req, res) => {
+    let {user, Comment } = req.body;
+    let id =  uuidv4();
+    posts.push({ id,user, Comment });
+    res.redirect("/post");
+})
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id == p.id);
+    res.render("showPost.ejs",{post});
 })
